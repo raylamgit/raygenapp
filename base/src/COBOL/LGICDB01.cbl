fd6331e2ba9910eca9133162c6ed5a6c3e2a4604
raylamgit <73551712+raylamgit@users.noreply.github.com>
2023-03-30 15:06:58 -0500

    Update LGICDB01.cbl

--- base/src/COBOL/LGICDB01.cbl
+++ base/src/COBOL/LGICDB01.cbl
@@ -18,7 +18,7 @@
       * Select customer details from DB2 table                         *
       *                                                                *
       *                                                                *
-      *   ray lam 03/30/2023 sysprintUtil 1,2
+      *   ray lam 03/30/2023 sysprintUtil 1,2,3
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LGICDB01.
