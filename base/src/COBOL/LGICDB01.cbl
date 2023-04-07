040d75e5c3430eb340759c78f7e02e65aa444057
Raymond Lam <raymond.lam@ibm.com>
2023-04-07 13:38:29 -0500

    after complist

--- base/src/COBOL/LGICDB01.cbl
+++ base/src/COBOL/LGICDB01.cbl
@@ -1,5 +1,5 @@
       ******************************************************************
-      *   ray lam 04/04/2023 compileUtil 1,2,3,4
+      *   ray lam 04/04/2023 compileUtil 1,2,3,4,5
       *                                                                *
       * LICENSED MATERIALS - PROPERTY OF IBM                           *
       *                                                                *
