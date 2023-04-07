82792b0ae3e1c4c798289bea3cfc79063f532551
Raymond Lam <raymond.lam@ibm.com>
2023-04-07 10:34:18 -0500

    removed member from LOAD

--- base/src/COBOL/LGICDB01.cbl
+++ base/src/COBOL/LGICDB01.cbl
@@ -1,5 +1,5 @@
       ******************************************************************
-      *   ray lam 04/04/2023 compileUtil 1
+      *   ray lam 04/04/2023 compileUtil 1,2
       *                                                                *
       * LICENSED MATERIALS - PROPERTY OF IBM                           *
       *                                                                *
